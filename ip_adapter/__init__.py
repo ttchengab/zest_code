9ae5c8a5a36b3cc5cb2d7e31aa7281a090e4acda
Tim Cheng <ttchengab@connect.ust.hk>
2024-04-03 01:56:39 +0100

    Delete ip_adapter directory

--- ip_adapter/__init__.py
+++
@@ -1,9 +0,0 @@
-from .ip_adapter import IPAdapter, IPAdapterPlus, IPAdapterPlusXL, IPAdapterXL, IPAdapterFull
-
-__all__ = [
-    "IPAdapter",
-    "IPAdapterPlus",
-    "IPAdapterPlusXL",
-    "IPAdapterXL",
-    "IPAdapterFull",
-]
